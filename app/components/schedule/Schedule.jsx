8542b7c2e2178c307a5b7bb7af7fe950f3fd5086
Gabriele Messina <g.messina@neoteksolutions.it>
2024-10-30 21:50:46 +0100

    Fix Schedule typos

--- app/components/schedule/Schedule.jsx
+++ app/components/schedule/Schedule.jsx
@@ -25,12 +25,12 @@ const Schedule = () => {
               <td className="py-2 px-3 text-left hover:bg-[#FF7EB5] hover:text-white" rowSpan='2'><span className="opacity-80">15:00 - 15:45</span><br /> <span className="md:ms-3 block">QFT (Schilirò)</span></td>
             </tr>
             <tr style={{height: '4rem', borderBottom: "1px solid lightgray"}}>
-              <td className="py-2 px-3 text-left hover:bg-[#FF7EB5] hover:text-white" style={{borderRight: "2px dashed #FF7EB5"}}><span className="opacity-80">15:15 - 15:55</span><br /> <span className="md:ms-3 block">Qiskti 101 (C. Viola)</span></td>
+              <td className="py-2 px-3 text-left hover:bg-[#FF7EB5] hover:text-white" style={{borderRight: "2px dashed #FF7EB5"}}><span className="opacity-80">15:15 - 15:55</span><br /> <span className="md:ms-3 block">Qiskit 101 (C. Viola)</span></td>
             </tr>
             <tr style={{height: '4rem', borderBottom: "1px solid lightgray"}}>
               <td className="py-2 px-3 text-left hover:bg-[#FF7EB5] hover:text-white" style={{borderRight: "2px dashed #FF7EB5"}} rowSpan='2'><span className="opacity-80">16:00 - 17:00</span><br /> <span className="md:ms-3 block">Quantum Basics (G. Piccitto)</span></td>
               <td className="py-2 px-3 text-left hover:bg-[#FF7EB5] hover:text-white" style={{borderRight: "2px dashed #FF7EB5"}}><span className="opacity-80">15:45 - 16:15</span><br /> <span className="md:ms-3 block">Master in Quantum Technologies Presentation (E. Paladino)</span></td>
-              <td className="py-2 px-3 text-left hover:bg-[#FF7EB5] hover:text-white"><span className="opacity-80">15:45 - 16:15</span><br /> <span className="md:ms-3 block">Presentazione Progetti</span></td>
+              <td className="py-2 px-3 text-left hover:bg-[#FF7EB5] hover:text-white"><span className="opacity-80">15:45 - 16:15</span><br /> <span className="md:ms-3 block">Qiskit Projects @UniCT</span></td>
             </tr>
             <tr style={{height: '4rem', borderBottom: "1px solid lightgray"}}>
               <td className="py-2 px-3 text-left hover:bg-[#FF7EB5] hover:text-white" style={{borderRight: "2px dashed #FF7EB5"}}><span className="opacity-80">16:15 - 17:00</span><br /> <span className="md:ms-3 block">TBA</span></td>
